012c175ecb4c3011e2d763ea3d71667a0943cc3d
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2023-12-19 15:25:19 +0530

    updated python coding!

--- test.py
+++ test.py
@@ -18,7 +18,7 @@ print("Num2:",num2)'''
 #print("Idiot"+" "+input("Enter Your Name:"))
 #print(input("Enter Your Name:")+" "+"idiot")
 #Biswise operators:-(&,|,^)
-a=25
+"""a=25
 b=15
 print(a & b)
 print(a | b)
@@ -27,4 +27,9 @@ print(bin(5))
 print(bin(10))
 print("& operator:",0b1000)
 print("| operator:",0B1000)
-print("^ operator:",0b0000)
+print("^ operator:",0b0000)"""
+#print(bool(input("Enter Boolean Values:")))
+#print(float(input("Enter Floting-point values:")))
+#print(int(input("Enter Integral values:")))
+#print(eval(input("Enter Any Values:")))
+#print(input("Enter String Values:"))
