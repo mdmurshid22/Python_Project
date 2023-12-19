9bea3cd4a4c3f4840456271d9470410286bf615c
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2023-12-19 14:40:16 +0530

    updated python coding!

--- test.py
+++ test.py
@@ -16,4 +16,10 @@ num1,num2=num2,num1
 print("Num1:",num1)
 print("Num2:",num2)'''
 #print("Idiot"+" "+input("Enter Your Name:"))
-print(input("Enter Your Name:")+" "+"idiot")
+#print(input("Enter Your Name:")+" "+"idiot")
+#Biswise operators:-(&,|,^)
+a=79
+b=90
+print(a & b)
+print(a | b)
+print(a ^ b)
