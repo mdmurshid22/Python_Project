277874c84ef9e0476cf3653d2b32fa505ba547c4
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2023-12-26 23:25:41 +0530

    update my python code

--- string.py
+++ string.py
@@ -1,1 +1,13 @@
 #A Collection of Characters or Group of Characters within single or double qoutes.
+#s="Python is a programming language and easy to programming"
+#print(s)
+'''sp=s.split()
+print(sp)
+j=" ".join(sp[::-1])
+print(j)
+j=" ".join(reversed(sp))
+print(j)'''
+eno=int(input("Enter Your Employee Number:"))
+ename=input("Enter Your Employee Name:")
+esalary=float(input("Enter Your Employee Salary:"))
+eaddress=input("Enter Your Employee Address:")
