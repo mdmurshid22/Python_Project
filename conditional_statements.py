500a4c499d9f75e7692ca0455426ffc5150383d4
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2023-12-28 15:08:52 +0530

    update my python code!

---
+++ conditional_statements.py
@@ -0,0 +1,8 @@
+num1=int(input("Enter First Number:"))
+num2=int(input("Enter Second Number:"))
+'''if num1 == num2:
+	print("Both Numbers are equal")
+elif num1>num2:
+	print("First Number is Greater:",num1)
+else:
+	print("Second Number is Greater:",num2)'''
