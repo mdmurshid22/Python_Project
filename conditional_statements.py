721d64d0c71d9a191fda5e515c92014872999e50
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2023-12-28 15:52:11 +0530

    update my python code!

--- conditional_statements.py
+++ conditional_statements.py
@@ -8,6 +8,7 @@ elif num2>num3:
 	print("Second Number is Greater:",num2)
 else:
 	print("Third Number is Greater:",num3)'''
+
 #2)Polindrome?
 '''s=input("Enter Some String:")
 s1=s[::-1]
@@ -15,12 +16,14 @@ if s == s1:
 	print("It is Polindrome:",s)
 else:
 	print("It is not a Polindrome:",s)'''
+
 #Voting?
 '''age=int(input("Enter Your Age:"))
 if age >= 18:
 	print("You are Eligible for voting:",age)
 else:
 	print("You are not Eligible for Voting:",age)'''
+
 #Pass or Fail program?
 '''tamil=int(input("Enter Tamil Mark:"))
 english=int(input("Enter English Mark:"))
@@ -35,4 +38,4 @@ if tamil >=35:
  					print("All Subjects Pass:","Tamil:{} English:{} Maths:{} Science:{} Social:{}".format(tamil,english,maths,science,social))
  					print("Percentage of All Subjects is:{:.1f}%".format(tamil,english,maths,science,social/5))
 else:
-	print("Fail:","Tamil:{} English:{} Maths:{} Science:{} Social:{}".format(tamil,english,maths,science,social))'''
+	print("Fail:","Tamil:{} English:{} Maths:{} Science:{} Social:{}".format(tamil,english,maths,science,social))''s'
