0c2032ac9b3f3f774838e0475b51504b802dbe07
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2023-12-29 16:13:52 +0530

    Update My Python Code!

--- conditional_statements.py
+++ conditional_statements.py
@@ -44,7 +44,7 @@ else:
 d=dir(math)
 for x in d:
 	print(x)'''
-num=int(input("How Many Subject Marks Do You Want:"))
+"""num=int(input("How Many Students Marks Do You Want:"))
 list=["Tamil","English","Maths","Science","Social"]
 for i in range(num):
 	l=[]
@@ -58,6 +58,119 @@ for i in range(num):
 		else:
 			print("Fail {} subject {}".format(l[x],list[x]))
 	print("Total Marks {} By {}".format(output,student_name))
-	print()
+	print()"""
+"""n=int(input("Enter Any Number 1 to 7:"))
+if n == 1:
+	print("Sunday")
+elif n == 2:
+	print("Monday")
+elif n == 3:
+	print("Tuesday")
+elif n == 4:
+	print("Wednesday")
+elif n == 5:
+	print("Thursday")
+elif n == 6:
+	print("Friday")
+elif n == 7:
+	print("Saturday")
+else:
+	print("Invalid!")
+city_name=input("Enter Your City Name:")
+city=city_name.strip()
+if city.upper() == "KARAIKAL":
+	print("KARAIKAL IS GOOD")
+elif city.upper() == "NAGORE":
+	print("NAGORE IS TOO GOOD")
+elif city.upper() == "NAGAPATTINAM":
+	print("NAGAPATTINAM IS VERY GOOD")
+else:
+	print("INVALID CITY!")"""
+degree_name=input("Enter Any degree[ENGINEERING,ARTS AND SCIENCE,DIPLOMA]: ")
+degree=degree_name.strip()
+if degree.upper() == "ENGINEERING":
+	department_name=input("Enter Any Department of Engineering[MECH,EEE,ECE,CSC]:")
+	department=department_name.strip()
+	if department.upper() == "MECH":
+		print("BOYS ONLY!")
+	elif department.upper() == "EEE":
+		print("BOTH BOYS AND GIRLS")
+	elif department.upper() == "ECE":
+		print("GIRLS ONLY")
+	elif department.upper() == "CSC":
+		print("FUN ONLY")
+	else:
+		print("INVALID DEPARTMENT")
+elif degree.upper() == "ARTS AND SCIENCE":
+	department_name=input("Enter Any Department of Arts and Science[BCA,BSC,CS,BBA]:")
+	department=department_name.strip()
+	if department.upper() == "BCA":
+		print("BOYS ONLY!")
+	elif department.upper() == "BSC":
+		print("BOTH BOYS AND GIRLS")
+	elif department.upper() == "CS":
+		print("GIRLS ONLY")
+	elif department.upper() == "BBA":
+		print("FUN ONLY")
+	else:
+		print("INVALID DEPARTMENT")
+elif degree.upper() == "DIPLOMA":
+	department_name=input("Enter Any Department Diploma[MECH,EEE,ECE,CSC]:")
+	department=department_name.strip()
+	if department.upper() == "MECH":
+		print("BOYS ONLY!")
+	elif department.upper() == "EEE":
+		print("BOTH BOYS AND GIRLS")
+	elif department.upper() == "ECE":
+		print("GIRLS ONLY")
+	elif department.upper() == "CSC":
+		print("FUN ONLY")
+	else:
+		print("INVALID DEPARTMENT")
+else:
+	print("INVALID DEGREE!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
