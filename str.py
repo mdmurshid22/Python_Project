a21fe03968b24e992ea80a200236284a3bad845c
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2024-01-24 09:59:03 +0530

    update my python coding!

---
+++ str.py
@@ -0,0 +1,16 @@
+'''string=input("Enter Any String:")
+i=-1
+output=""
+l=-len(string)-1
+while i > l:
+	output=output+string[i]
+	i-=1
+print(output)
+num=int(input('Enter Any Number:'))
+for i in range(num+1):
+	print('* '*i)
+num=int(input('Enter Any Number'))
+i=1
+while i <= num:
+	print("* "*i)
+	i+=1'''
