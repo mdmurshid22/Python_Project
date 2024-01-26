bee3021c57272dde049adefbbb58455adc89bb64
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2024-01-26 18:57:05 +0530

    updated python coding!

--- workout.py
+++ workout.py
@@ -1,10 +1,10 @@
-#inner=int(input('Enter Inner-Loop Number:'))
-#outer=int(input('Enter Outer-Loop Number:'))
+outer=int(input('Enter Outer-Loop Number:'))
+inner=int(input('Enter Inner-Loop Number:'))
 i=1
 j=1
-while i <= 5:
+while i <= outer:
 	print(i,'Outer_Loop')
-	while j <= 5:
+	while j <= inner:
 		print(j,'Inner_Loop')
 		j+=1
 	j=1
