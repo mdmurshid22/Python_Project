ae59aa256db9713787d551c14ee6f1ee09166c3f
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2024-01-26 18:58:13 +0530

    updated python coding!

--- str.py
+++ str.py
@@ -60,3 +60,14 @@ while True:
 		print(name)
 -----------------------------------------------------------------------------------------------------------------
 '''
+outer=int(input('Enter Outer-Loop Number:'))
+inner=int(input('Enter Inner-Loop Number:'))
+i=1
+j=1
+while i <= outer:
+	print(i,'Outer_Loop')
+	while j <= inner:
+		print(j,'Inner_Loop')
+		j+=1
+	j=1
+	i+=1
