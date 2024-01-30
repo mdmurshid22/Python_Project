010bd968ef9f856026798267ba1349d90a1fd1af
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2024-01-30 19:31:56 +0530

    update list data structure coding!

--- list.py
+++ list.py
@@ -1,4 +1,4 @@
-list=[7,8.8,True,False,5+9J,'k',"idiot"]
+'''list=[7,8.8,True,False,5+9J,'k',"idiot"]
 print(type(list))
 print(list)
 print(list.append(['kk','kkk']))#To add the element at last only and only one value.
@@ -43,3 +43,70 @@ print(list.pop(3))#To remove a particular index position of a list.
 print(list)
 print(list.remove(''))#To remove first element of the given list.
 print(list)
+print(dir(list))
+------------------------------------------------------------------------------------------------------------------------------
+list=[10,20,[30,40,[111,[60,70,80],90,100],111],222,333]
+print(list)
+list[2].pop()
+list[2][2][1][2]='idiot'
+list[2][2][1][1]=222
+list[2][2].insert(2,577)
+print(list)
+----------------------------------------------------------------------------------------------------------------------------------
+list=[3,1,5,2,6,2,9,6,7,9]
+i=0
+l=0
+while i <= len(list):
+	l=i
+	i+=1
+print(list)
+print(l)
+---------------------------------------------------------------------------------------------------------------------------------
+list=[1,3,5,8,2,14,7,6]
+i=0
+l=[]
+while i < len(list):
+	if list[i]%2 == 0:
+		l.append(list[i])
+	i+=1
+print(list)
+print(l)
+--------------------------------------------------------------------------------------------------------------------------------
+li=[3,10,2,5,8,22,15,4,16,20,25]
+l2=[]
+l5=[]
+a=0
+while a < len(li):
+	if li[a]%2 == 0:
+		l2.append(li[a])
+	if li[a]%5 == 0:
+		l5.append(li[a])
+	a+=1
+print('list is:{}'.format(li))
+print('Divisible by 2 is:{}'.format(l2))
+print('Divisible by 5 is:{}'.format(list(l5)))
+-----------------------------------------------------------------------------------------------------------------------------------
+l=eval(input("Enter Any Sequences:"))
+print(type(l))
+print('Original List is:{}'.format(l))
+s=set(l)
+print(list(s))
+print(type(list(s)))
+------------------------------------------------------------------------------------------------------------------------------------
+l=eval(input("Enter Any Sequences:"))
+l1=[]
+for x in l:
+	if x not in l1:
+		l1.append(x)
+print(l1)
+-------------------------------------------------------------------------------------------------------------------------------------
+l=eval(input("Enter Any Sequences:"))
+i=0
+j=[]
+while i < len(l):
+	if l[i] not in j:
+		j.append(l[i])
+	i+=1
+print('The Original Sequences is:{}'.format(l))
+print("Remove the duplicates are:{}".format(j))
+-------------------------------------------------------------------------------------------------------------------------------------'''
