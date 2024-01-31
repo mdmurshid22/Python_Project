b9327f6e5efac79ecd8a6068f60ca0cd57b90907
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2024-01-31 15:16:43 +0530

    list data structure practise code is updated!

--- tuple.py
+++ tuple.py
@@ -1,4 +1,4 @@
-tuple=(7,9.7,True,8+9J,True,False,True)
+'''tuple=(7,9.7,True,8+9J,True,False,True)
 print(type(tuple))
 print(len(tuple))
 print(tuple)
@@ -13,3 +13,40 @@ print(tuple.index(True))#To search the value and return the specified index or T
 i=tuple.index(7)#Syntax->t.index(value)
 print(i)
 print(dir(tuple))
+a=9
+b=8
+print(a)
+print(b)
+a,b=b,a
+print(a)
+print(b)
+----------------------------------------------------------------------------------------------------------------------------
+a='Python'
+i=-1
+l=[]
+l1=-len(a)
+while i >= l1:
+	l.append(a[i])
+	i-=1
+for x in l:
+	print(x,end="")
+-----------------------------------------------------------------------------------------------------------------------------
+a=12345
+aa=str(a)
+output=0
+i=0
+while i < len(aa):
+	output=output+int(aa[i])
+	i+=1
+print(output)
+------------------------------------------------------------------------------------------------------------------------------
+i=int(input("Enter Any Number:"))
+if (i%3 == 0) and (i%5 == 0):
+	print('bye')
+elif i%3 == 0:
+	print('hello')
+elif i%5 == 0:
+	print('hii')
+else:
+	print('Not Divisible by 3 and 5')
+-------------------------------------------------------------------------------------------------------------------------------'''
