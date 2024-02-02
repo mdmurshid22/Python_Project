9c7f5e969fa740a40d2fa1381b9014d3d814e6d4
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2024-02-02 15:21:05 +0530

    set date structure coding is updated!

--- set.py
+++ set.py
@@ -5,5 +5,13 @@ s=set()
 print(s)
 print(type(s))
 print(dir(set))
-print(s.add('k'))#set method() is add one elements to the set.
+print(s.add('k'))#None.
+#set method(exactly one argument) is add one elements to the set.
 print(s)
+s.clear()
+#clear Method is to clear all elements from the set.
+print(s)
+s1=set1.copy()
+print(s1)
+#copy Method is to copy the all elements from the set to create new object.
+print(set1.copy())
