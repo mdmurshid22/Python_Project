9417fc2e39aafcf0c00c523d239f2c27246a9027
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2024-02-12 16:28:17 +0530

    functions folder is created!

--- func.py
+++ func.py
@@ -1,6 +1,20 @@
-def square(n):
-	return n ** 2
-s=square(6)
-s1=square(7)
-print('The square of this number is:{}'.format(s))
-print('The square of this number is:{}'.format(s1))
+'''l=[]
+def fun(name=''):
+	return name
+f=fun(name=input('enter some string:'))
+for x in f:
+	l.append(x)
+print(l)'''
+def func(i):
+	l=[]
+	while True:	
+		if i >= 0:
+			l.append(i)
+			i=int(input('enter some number:'))
+			continue
+		elif i >= -1:
+			break
+		break
+	print(l)
+i=int(input('Enter Any Number:'))
+func(i)
