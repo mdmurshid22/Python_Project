ec35d58eca7859a60917bc273a86f4c3822b5829
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2024-02-14 16:34:51 +0530

    rename function folder

--- methods.py
+++ methods.py
@@ -9,9 +9,9 @@ for x in enumerate(a):
 print(dict(l))
 a=['a','b','c','d','f']
 b=[6,7,8,9,10]
-print(dict(zip(a[::-1],b[::-1])))'''
+print(dict(zip(a[::-1],b[::-1])))
 a=['a','b','c','d']
 b=[1,2,3,4]
 for x in range(len(a)):
 	d=(x,a[x],b[x])
-	print(tuple(d))
+	print(tuple(d))'''
