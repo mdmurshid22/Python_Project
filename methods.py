c5a0c9eb5b521a430610bdfe5e2864003066c306
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2024-02-14 16:28:35 +0530

    rename function folder

--- methods.py
+++ methods.py
@@ -12,10 +12,6 @@ b=[6,7,8,9,10]
 print(dict(zip(a[::-1],b[::-1])))'''
 a=['a','b','c','d']
 b=[1,2,3,4]
-l=tuple(enumerate(a))
-l1=[]
-for x,y in l:
-	l1.append(x)
-	l1.append(y)
-	l1.append(b[x])
-print(tuple(l1))
+for x in range(len(a)):
+	d=(x,a[x],b[x])
+	print(tuple(d))
