7c6812d0c2c652a12a7028a13ed6e548ef460037
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2024-02-14 15:01:38 +0530

    eval function coding!r

--- methods.py
+++ methods.py
@@ -1,3 +1,5 @@
-print(eval("'idiot'+'idiot'"))
+'''print(eval("'idiot'+'idiot'"))
 a=['one','two','three','one']
-print(dict(enumerate(a[::-1],10)))
+print(dict(enumerate(a[::-1],10)))'''
+for x,y in enumerate(range(1,11)):
+	print(x,y)
