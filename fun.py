8d2f272affcc76a88401f6396e1ff8e672173eb2
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2024-02-14 13:47:03 +0530

    rename function folder

---
+++ fun.py
@@ -0,0 +1,9 @@
+def my_fun(a,b,c):
+	return a,b,c
+a=int(input('enter first number:'))
+b=int(input('enter second number:'))
+c=int(input('enter third number:'))
+#r=my_fun(a,b,c)
+#print(type(r))
+#print(r)
+#print(type(my_fun(a,b,c)))
