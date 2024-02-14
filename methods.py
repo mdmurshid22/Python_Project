0a9da527e0dc25bdbb4c78b794901d4e3b6c395d
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2024-02-14 15:42:34 +0530

    rename function folder

--- methods.py
+++ methods.py
@@ -1,9 +1,9 @@
 '''print(eval("'idiot'+'idiot'"))
 a=['one','two','three','one']
-print(dict(enumerate(a[::-1],10)))'''
+print(dict(enumerate(a[::-1],10)))
 a=['a','b','c','d']
 #print(dict(enumerate(a)))
 l=[]
 for x in enumerate(a):
 	l.append(x)
-print(dict(l))
+print(dict(l))'''
