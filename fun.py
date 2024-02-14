b83bf5669d153fb3f6ac4745bf443ed1db254480
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2024-02-14 14:14:12 +0530

    rename function folder

--- fun.py
+++ fun.py
@@ -9,7 +9,7 @@ print(r)
 print(type(my_fun(a,b,c)))
 my_fun(a,b,c)
 print(my_fun(a,b,c))
-print(type(my_fun(a,b,c)))'''
+print(type(my_fun(a,b,c)))
 def my_fun(r='rollno',n='name',a='age',m='mark',ad='address'):
 	print('Roll no:{} Name:{} Age:{} Mark:{} and Address:{}'.format(r,n,a,m,ad))
 r=int(input('enter your roll no:'))
@@ -17,4 +17,4 @@ n=input('enter your name:')
 a=int(input('enter your age:'))
 m=int(input('enter your mark:'))
 ad=input('enter your address:')
-my_fun(r,n,a,m,ad)
+my_fun(r,n,a,m,ad)'''
