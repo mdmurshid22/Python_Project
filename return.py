cbdb204634b5a9c38b08b9aeb9684dff68b8ba37
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2024-02-15 15:31:50 +0530

    return statement folder is created!

--- return.py
+++ return.py
@@ -3,3 +3,16 @@
 a=int(input('enter a value:'))
 b=int(input('enter b value:'))
 print(add(a,b))'''
+s='PyThonLanguage'
+print(s)
+l=len(s)-1
+i=0
+l1=[]
+while i < l:
+	if s[i].isupper():
+		l1.append(ord(s[i])+32)
+	else:
+		l1.append(ord(s[i])-32)
+	i+=1
+for x in l1:
+	print(chr(x),end="")
