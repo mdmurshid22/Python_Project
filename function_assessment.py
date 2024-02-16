acbf2fc01e0226eee3f21050918e5ad39d95dda3
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2024-02-16 17:03:00 +0530

    function assessment coding!

--- function_assessment.py
+++ function_assessment.py
@@ -81,7 +81,7 @@ while i < len(a):
 print(l)
 ---------------------------------------------------------------------------------------------------------------
 8. Define a function in python that accepts 3 values and returns the maximum of three
-numbers.'''
+numbers.
 def max(n1,n2,n3):
 	if n1 > n2 and n1 > n3:
 		return n1
@@ -92,4 +92,4 @@ def max(n1,n2,n3):
 n1=int(input('enter first number:'))
 n2=int(input('enter second number:'))
 n3=int(input('enter third number:'))
-print(max(n1,n2,n3))
+print(max(n1,n2,n3))'''
