90d288838cd692da2c35dfccc026b0538b346cde
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2024-02-16 17:09:01 +0530

    function assessment coding!

--- function_assessment.py
+++ function_assessment.py
@@ -92,4 +92,5 @@ def max(n1,n2,n3):
 n1=int(input('enter first number:'))
 n2=int(input('enter second number:'))
 n3=int(input('enter third number:'))
-print(max(n1,n2,n3))'''
+print(max(n1,n2,n3))
+---------------------------------------------------------------------------------------------------------------'''
