18e7ee3623824fecb6e3d8d427a533ee8e089b54
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2024-02-22 15:38:46 +0530

    create and update the folder!

--- closure.py
+++ closure.py
@@ -1,13 +1,23 @@
-def f1(message):
+'''def f1(message):
 	msg=message
 	print('outer function')
 	def f2():
 		print('k')
 		print('inner function')
 		print(message)
-	return f2
+	return f1
 f=f1('idiot')
 f()
 print()
 del f1
-print(f())
+print(f())'''
+def add():
+	a='hii'
+	b=a
+	def sub():
+		a='hello'
+		#print(a)
+	sub()
+	print(b)
+	print(a)
+add()
