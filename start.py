e53abf0a4d0eb04d858dc9a565aebd297f44cee9
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2024-03-12 15:11:44 +0530

    updated python code!!

--- start.py
+++ start.py
@@ -27,7 +27,7 @@ class Per(Person):
 		super().__init__(a,b)
 		self.c = c
 	def home1(self):
-		print(self.a+self.b+self.c)
+		print(self.a*self.b*self.c)
 	def home2(self):
 		print(self.c)
 b=Per(90,80,70)
