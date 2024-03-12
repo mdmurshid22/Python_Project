bcf3589751d721fbb8b6e3941fb82036927f8212
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2024-03-12 15:11:07 +0530

    updated python folder!

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
-class Person:
+'''class Person:
 	def __init__(self,name,age):
 		self.name = name
 		self.age = age
@@ -14,4 +14,22 @@ class Per(Person):
 		print(self.address)
 b=Per('idiot',22,'karaikal')
 b.home()
+b.home1()'''
+
+class Person:
+	def __init__(self,a,b):
+		self.a = a
+		self.b = b
+	def home(self):
+		print(self.a+self.b)
+class Per(Person):
+	def __init__(self,a,b,c):
+		super().__init__(a,b)
+		self.c = c
+	def home1(self):
+		print(self.a+self.b+self.c)
+	def home2(self):
+		print(self.c)
+b=Per(90,80,70)
 b.home1()
+b.home2()
