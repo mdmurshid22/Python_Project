3fd8a1e15cfe16a82571ed323e86612e9232e047
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2024-03-26 15:11:32 +0530

    testpress code is updated!

--- file_handling.py
+++ file_handling.py
@@ -1,3 +1,8 @@
 with open('closure.py','r') as c:
-	for x in c.readlines():
+	r=c.read()
+#	for x in c.readlines():
+#		print(x)
+#	for x in range(5):
+#		print(c.readline())
+	for x in reversed(r):
 		print(x)
