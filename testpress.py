fa5bf8af04717a17e96cd6b19a041cede1d29a81
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2024-03-26 14:21:31 +0530

    testpress code is updated!

---
+++ testpress.py
@@ -0,0 +1,48 @@
+'''1. Write a program to print the reverse of the given string.
+Input
+codecode
+Output
+edocedoc
+Input Constraints
+1<=Length of string<=100
+Input string contains only lowercase characters ['a' to 'z'].
+
+2. Given a number, check whether it is a prime number or not.
+Input 1
+3
+Output 1
+Yes
+Input 2
+4
+Output 2
+No
+
+3. Given an array of numbers, arrange them in a way that it forms the largest value.
+Input
+[54, 546, 548, 60]
+Output
+6054854654
+
+Note
+The arrangement 6054854654 gives the largest value.
+Constraints
+1<=N<=1000
+1<=Number<=1000000
+
+4. Given a number N, print reverse of number N.
+Input
+988
+Output
+889
+Note
+Do not print leading zeros in output.
+For example N = 100
+Reverse of N should be 1 not 001.
+Constraints
+1<=N<=10000
+
+5. Given an array of numbers, find the maximum and minimum element in the array.
+Input
+[54, 546, 548, 60]
+Output
+548 54'''
