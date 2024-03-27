1a2f7d20c04977c0928707124f97dda5d10556bf
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2024-03-27 15:25:29 +0530

    updated a python code!

--- thread.py
+++ thread.py
@@ -1,2 +1,28 @@
-from copy import *
-print(dir())
+from threading import *
+def M():
+	print('Good Morning.')
+
+def A():
+	print('Good Afternoon.')
+
+def E():
+	print('Good Evening.')
+
+def N():
+	print('Good Night.')
+
+a1 = Thread(target = M)
+
+a2 = Thread(target = E)
+
+a3 = Thread(target = N)
+
+a4 = Thread(target = A)
+
+a1.start()
+
+a2.start()
+
+a3.start()
+
+a4.start()
