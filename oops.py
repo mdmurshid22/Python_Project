7088e1dfcf007f23f82903baba19b06548304a22
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2024-05-16 06:15:17 +0530

    new oops concept folder is created successfully!

--- oops.py
+++ oops.py
@@ -1,5 +1,9 @@
 class k:
 	'This is my idiot name'
 
-print(k.__doc__)
-help()
+murshid = k()
+idiot = k()
+murshid.name = 'm'
+idiot.name = 'h'
+print('This is:',idiot.name)
+print(murshid.__dict__)
