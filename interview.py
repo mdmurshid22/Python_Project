276682cd495fd5cfb09859d50206acb649250cd5
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2024-07-22 20:12:24 +0530

    Prime Number Coding Practise!

---
+++ interview.py
@@ -0,0 +1,28 @@
+#1) i)Write a python program to given number is prime or not?
+
+'''num = int(input("Enter some number:"))
+i = 2
+prime = True
+while(i<num):
+	if(num%i == 0):
+		print('{} is not prime number'.format(num))
+		prime = False
+		break
+	i+=1
+if(prime == True):
+	print('{} is prime number'.format(num))'''
+
+#---------------------------------------------------------------------------------
+
+#1) ii)write a python program to given first to Nth prime number?
+
+
+
+
+
+
+
+
+
+
+
---
+++ one.py
@@ -0,0 +1,11 @@
+'''name = input('Enter My Heart:')
+while(name != 'baliga'):
+	name = input('Enter My Sweet:')
+print('Uyir:', name)'''
+#--------------------------------------------------------
+'''name = input("Enter Any Name:")
+i = 0
+while(i < len(name)):
+	print(i,'=',name[i])
+	i+=1'''
+#--------------------------------------------------------
