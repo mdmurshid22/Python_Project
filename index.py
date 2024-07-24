f492d9566beea477bbbb290e50dbf752ee65d45b
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2024-07-24 14:56:13 +0530

    code practise!

--- index.py
+++ index.py
@@ -1,1 +1,13 @@
-#1) Write a python program to given number is prime or not?
+#1) Write a python program to given number is prime or not by using while loop?
+
+number = int(input('enter some number:'))
+i = 2
+prime = True
+while(i<number):
+	if(number%i == 0):
+		print(number, 'not a prime number.')
+		prime = False
+		break
+	i=i+1
+if(prime == True):
+	print(number, 'is a prime number.')
