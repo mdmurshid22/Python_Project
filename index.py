43b6038e0f98a30431d063e7065ea6ae8a1a257c
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2024-07-24 16:29:20 +0530

    for loop with prime number!

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 #1) Write a python program to given number is prime or not by using while loop?
 
-number = int(input('enter some number:'))
+'''number = int(input('enter some number:'))
 i = 2
 prime = True
 while(i<number):
@@ -10,4 +10,16 @@ while(i<number):
 		break
 	i=i+1
 if(prime == True):
-	print(number, 'is a prime number.')
+	print(number, 'is a prime number.')'''
+
+#1) for loop?
+
+number = int(input('enter some number:'))
+prime = True
+for x in range(2,number,1):
+	if(number%x == 0):
+		print(number,'not a prime number.')
+		prime = False
+		break
+if(prime == True):
+	print(number,'is prime number.')
