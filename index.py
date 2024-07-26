4ceaac846fe042af61f4d86ec464bf1f8782d007
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2024-07-26 19:47:23 +0530

    prime number code inpython!

--- index.py
+++ index.py
@@ -14,7 +14,7 @@ if(prime == True):
 
 #1) for loop?
 
-number = int(input('enter some number:'))
+'''number = int(input('enter some number:'))
 prime = True
 for x in range(2,number,1):
 	if(number%x == 0):
@@ -22,4 +22,33 @@ for x in range(2,number,1):
 		prime = False
 		break
 if(prime == True):
-	print(number,'is prime number.')
+	print(number,'is prime number.')'''
+
+#2) write a python program to find the N number of prime number by using while loop?
+
+number = int(input('enter N number of prime number:'))
+i = 2
+prime = True
+num = 3
+print('prime number are:')
+print(2)
+count = 1
+while(count<=number):
+	while(i<num):
+		if(num%i == 0):
+			prime = False
+			break
+		i=i+1
+	if(prime == True):
+		print(num)
+		count+=1
+	num+=2
+
+
+
+
+
+
+
+
+
