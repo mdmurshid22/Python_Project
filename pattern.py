8f8cf14cb50bc078d1923391837056741c07a11a
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2024-08-04 11:58:18 +0530

    Right triangle pattern programs in python.

--- pattern.py
+++ pattern.py
@@ -13,11 +13,30 @@ for i in range(1,num+1):
 		print('* '*num)
 	else:
 		print('*'+'  '*(num-2),'*')'''
-
 #----------------------------------------------------------------------------------
 
-#3)Right triangle pattern in python.
+#3)Right triangle pattern program in python.
 
-num = int(input('Enter any Number of Rows:'))
+'''num = int(input('Enter any Number of Rows:'))
 for i in range(1,num+1):
-	print(' '*(num-i),'*'*(i))
+	print(' '*(num-i),'*'*(i))'''
+#----------------------------------------------------------------------------------
+
+#4)Left triangle pattern program in python.
+
+num = int(input('Enter Any Number of Rows:'))
+for i in range(1,num+1):
+	print('*'*i)
+
+
+
+
+
+
+
+
+
+
+
+
+
