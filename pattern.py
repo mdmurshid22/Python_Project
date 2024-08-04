41303466304215f14c252f78ee10c92a3bf4d903
mdmurshid22 <140339570+mdmurshid22@users.noreply.github.com>
2024-08-04 11:41:57 +0530

    Hollow Square pattern programs in python.

--- pattern.py
+++ pattern.py
@@ -1,5 +1,15 @@
 #1)Square star pattern in python.
 
+'''num = int(input('Enter Any Number of Rows:'))
+for i in range(1,num+1):
+	print('* '*num)'''
+#----------------------------------------------------------------------------------
+
+#2)Hollow square pattern program in python.
+
 num = int(input('Enter Any Number of Rows:'))
 for i in range(1,num+1):
-	print('* '*num)
+	if(i==1) or (i==num):
+		print('* '*num)
+	else:
+		print('*'+'  '*(num-2),'*')
